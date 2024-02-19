4f18297565ab6c69753ab68c42cf9418a820d928
kr1sty <ads.lombardi@gmail.com>
2024-02-19 19:02:05 +0100

    fixed null head

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: chrlomba <chrlomba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 22:48:52 by chrlomba          #+#    #+#             */
-/*   Updated: 2024/02/19 18:30:52 by chrlomba         ###   ########.fr       */
+/*   Updated: 2024/02/19 19:01:31 by chrlomba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ t_int_list	*arg_list_parsing(int argc, char **argv)
 {
 	t_int_list	*stack;
 	t_int_list	*new_node;
-	int		num;
-	int		i;
+	int			num;
+	int			i;
 
-	i = 1;
-	stack = malloc(sizeof(t_int_list *));
+	i = 2;
+	stack = ft_int_lstnew(ft_atoi(argv[1]));
 	if (!stack)
 		return (NULL);
 	while (i < argc)
@@ -42,13 +42,13 @@ t_int_list	*arg_str_parsing(char **argv)
 	char		**split;
 	int			i;
 
-	i = 0;
-	stack = malloc(sizeof(t_int_list *));
-	if (!stack)
-		return (NULL);
+	i = 1;
 	split = ft_split(argv[1], ' ');
 	if (!split)
-		ft_int_lstclear(stack);
+		perror("Error\n");
+	stack = ft_int_lstnew(ft_atoi(split[0]));
+	if (!stack)
+		return (NULL);
 	while (split[i])
 	{
 		new_node = ft_int_lstnew(ft_atoi(split[i]));
